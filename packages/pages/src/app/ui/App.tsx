00b695cfb6126d22ec97ea6b36a66e7faaa59f91
William Wong <compulim@users.noreply.github.com>
2023-11-28 21:25:50 +0000

    Update action versions

--- packages/pages/src/app/ui/App.tsx
+++ packages/pages/src/app/ui/App.tsx
@@ -1,7 +1,17 @@
 import './App.css';
 
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
+
+const { createDirectLine, ReactWebChat } = window['WebChat'];
 
 export default memo(function App() {
+  const [directLine, setDirectLine] = useState<any>(undefined);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    return () => abortController.abort();
+  }, []);
+
   return <h1>Hello, World!</h1>;
 });
